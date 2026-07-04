0aaa040520c2464bec7dd5ce8ef22153d564937f
Atharv Hirve <atharvhirve14@gmail.com>
2026-07-04 22:10:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -63,5 +63,8 @@ def ask():
 def home():
     return "Saarthi Backend Running ✅"
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
